3720ead9b7c987c39dd961d2b1c3cbdf2e0819bb
Aoy Natthaphong <nath.wangsungnone@gmail.com>
2025-05-03 18:12:08 +0700

    update

--- resources/views/admin/tour_management/tour_section_management/tour_section_management.blade.php
+++ resources/views/admin/tour_management/tour_section_management/tour_section_management.blade.php
@@ -3,9 +3,9 @@
 
 <h3 class="text-center">Tour Section Management</h3><br>
 
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+{{-- <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
     Create New Tour Section
-</button>
+</button> --}}
 
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" style="margin-top: 5%;">
